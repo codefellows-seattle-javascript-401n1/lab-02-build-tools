68b684f5cbb16c4d2f384f8e10d103b82c6a3c8f
Doug Popadince <gsmatth@icloud.com>
2016-05-01 09:41:30 -0700

    added require statements for gulp-mocha and gulp-eslint

---
+++ lab-doug/gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var mocha = require('gulp-mocha');
+
+gulp.task('mocha-test', function(){
+  //source is greet-test.js
+  //run command mocha greet-test.js
+  //source is all files in lab-doug except node_modules folder
+  //stream it through eslint
+});
+gulp.task('eslint', function(){
+  //source is greet-test.js
+  //run command mocha greet-test.js
+  //source is all files in lab-doug except node_modules folder
+  //stream it through eslint
+});
