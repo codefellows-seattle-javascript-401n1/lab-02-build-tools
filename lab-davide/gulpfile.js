3ea4038a13bd349ac7b519de30f4208371e814e9
FazioD <faziod@uw.edu>
2016-05-01 11:12:37 -0700

    adding bonus 1

--- lab-davide/gulpfile.js
+++ lab-davide/gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
+
 gulp.task('default', function() {
   return gulp.src('./test/greet-test.js', {read: false})
   .pipe(mocha({reporter: 'nyan'}));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('./test/greet-test.js', ['default']);
+});
+ 
