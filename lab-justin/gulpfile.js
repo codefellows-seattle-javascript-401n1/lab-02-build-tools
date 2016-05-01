c77698e7e0688e9ddc629f4b66aaca82826fcebe
Justin <klofseattle@hotmail.com>
2016-05-01 11:18:58 -0700

    first commit

---
+++ lab-justin/gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const eslint = require('gulp-eslint');
+
+
+gulp.task('default', function(){
+
+  return gulp.src('test/*.js')
+  .pipe(mocha());
+});
+
+gulp.task('eslint', function(){
+
+  return gulp.src('*.js')
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+var watcher = gulp.watch('*.js', !'package.json');
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+});
