0f65972f2be05bd0b0fe1e7b673d14697e43639a
Matthew Smith <mattsmith203@hotmail.com>
2016-05-01 10:20:24 -0700

    changed lint task to include all files except for node modules

--- lab-matt/gulpfile.js
+++ lab-matt/gulpfile.js
@@ -5,7 +5,7 @@ const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
 gulp.task('lint', function(){
-  return gulp.src(['index.js', 'lib/*.js', 'test/*.js'])
+  return gulp.src(['**/*.js','!node_modules/**'])
   .pipe(eslint()).pipe(eslint.format());
 });
 
