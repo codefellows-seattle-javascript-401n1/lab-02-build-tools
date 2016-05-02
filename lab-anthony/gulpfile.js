f11b12c07627212eb6e767eb0f61cb181e6670a4
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-01 19:29:18 -0700

    move eslintrc options to gulp task, add gitignore

--- lab-anthony/gulpfile.js
+++ lab-anthony/gulpfile.js
@@ -5,15 +5,54 @@ const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
 
 gulp.task('default', function(){
-  return gulp.src(['*.js', 'test/*.js', '!package.json'])
+  return gulp.src(['*.js', 'test/*.js'])
             .pipe(mocha())
-            .pipe(eslint())
+            .pipe(eslint(
+              {
+                'rules':{
+                  'no-console': 0,
+                  'indent': [
+                    2,
+                    2
+                  ],
+                  'quotes': [
+                    2,
+                    'single'
+                  ],
+                  'linebreak-style': [
+                    2,
+                    'unix'
+                  ],
+                  'semi': [
+                    2,
+                    'always'
+                  ]
+                },
+                'env': {
+                  'es6': true,
+                  'node': true,
+                  'browser': true
+                },
+                'globals': {
+                  'describe': false,
+                  'it': false,
+                  'beforeEach': false,
+                  'afterEach': false,
+                  'before': false,
+                  'after': false
+                },
+                'ecmaFeatures': {
+                  'modules': true,
+                  'experimentalObjectRestSpread': true,
+                  'impliedStrict': true
+                },
+                'extends': 'eslint:recommended',
+                'useEslintrc':false
+              }
+            ))
             .pipe(eslint.format());
 });
 
 gulp.task('watch', function(){
-  const watcher = gulp.watch(['*.js', 'test/*.js', '!package.json'], ['default']);
-  watcher.on('change', function(e){
-    console.log('The file '+e.path+' was '+e.type+', running tasks again:');
-  });
+  gulp.watch(['*.js', 'test/*.js', '!package.json'], ['default']);
 });
