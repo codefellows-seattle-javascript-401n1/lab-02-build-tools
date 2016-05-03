4093d3e5b208dff2c5b5d793faf83739a4fb5c54
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-05-02 19:42:58 -0700

    add ./

---
+++ lab-yunjoo/lib/greet.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports.greet = function(name){
+  const input = name || 'yunjoo' ;
+  return 'hello ' + input;
+};
+//
+// greet();
+// exports.greet = greet;
+// // greeting(process.argv[2]);
