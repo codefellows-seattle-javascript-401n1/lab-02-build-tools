17b7097da3a166b9f5abdd890261cf236a3d619a
kyleschnirring <kyleschnirring@gmail.com>
2016-05-03 21:37:22 -0700

    just a commit

--- lab-kyle/test/hello-test.js
+++ lab-kyle/test/hello-test.js
@@ -1,11 +1,11 @@
 'use strict';
 const hello = require(__dirname + '/../lib/greet');
-const assert = require('assert');
+const  = require('assert');
 
 describe('testing module lib/greet.js', function(){
   describe('testing function greet()', function(){
     it('should return hello world', function(){
-      assert.equal(hello.greet(), 'hello world');
+      expect(hello.bufferfiles()).to.equal(new Buffer);
     });
   });
 });
