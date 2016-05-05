dcd4d214cc95d4729c6725939da876babfe78c2f
kyleschnirring <kyleschnirring@gmail.com>
2016-05-04 17:48:54 -0700

    got it running

--- lab-kyle/test/hello-test.js
+++ lab-kyle/test/hello-test.js
@@ -1,11 +1,11 @@
 'use strict';
 const hello = require(__dirname + '/../lib/greet');
-const  = require('assert');
+const assert = require('assert');
 
 describe('testing module lib/greet.js', function(){
   describe('testing function greet()', function(){
     it('should return hello world', function(){
-      expect(hello.bufferfiles()).to.equal(new Buffer);
+      assert.equal(hello.greet(), 'hello world');
     });
   });
 });
